c991d4b860ce44ff8e8186046dd673f2e6da19e3
Saliim17 <ahouchsalim@gmail.com>
2022-11-03 16:38:14 +0100

    Added text input to filter API data

--- lib/screens/spain/spain_screen.dart
+++ lib/screens/spain/spain_screen.dart
@@ -1,7 +1,10 @@
-import 'package:calendarific/screens/spain/spain_body_screen.dart';
+import 'package:calendarific/screens/france/france_body_screen.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:get/get.dart';
 
+import '../../controllers/api_controller.dart';
 import '../../utils/dimensions.dart';
 import '../../widgets/big_text.dart';
 
@@ -13,6 +16,33 @@ class SpainScreen extends StatefulWidget {
 }
 
 class _SpainScreenState extends State<SpainScreen> {
+
+  ApiController apiController = Get.put(ApiController());
+
+  // use this controller to get what the usd typed
+  final _textController = TextEditingController();
+
+  // store user text input into variable
+  String userInput = "";
+
+  _loadResource (BuildContext context) {
+
+    if (userInput == "")  {
+      apiController.setYear("2022");
+    } else {
+      apiController.setYear(userInput);
+    }
+    apiController.setCountry("ES");
+    apiController.fetchData();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance
+        .addPostFrameCallback((_) =>_loadResource(context));
+  }
+
   @override
   Widget build(BuildContext context) {
 
@@ -28,7 +58,7 @@ class _SpainScreenState extends State<SpainScreen> {
         children: [
           //showing the header
           Container(
-            color: Colors.lightBlue,
+            color: CupertinoColors.destructiveRed,
             margin: EdgeInsets.only(
                 top: Dimensions.height45, bottom: Dimensions.height15
             ),
@@ -47,14 +77,72 @@ class _SpainScreenState extends State<SpainScreen> {
                 ]
             ),
           ),
+          Container(
+              margin: EdgeInsets.all(Dimensions.height10),
+              child: BigText(text: "Holidays in Spain!", fontWeight: FontWeight.w700)
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: Dimensions.height15),
+            child: Container(
+              width: Dimensions.screenWidth * 0.9,
+              height: Dimensions.screenHeight / 14,
+              decoration: BoxDecoration(
+                color: const Color.fromRGBO(240, 240, 240, 1),
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Row(
+                children: [
+                  InkWell(
+                      onTap: () {
+                        setState(() {
+                          userInput = _textController.text;
+                          apiController.setYear(userInput);
+                          apiController.fetchData();
+                        });
+                      },
+                      child: Padding(
+                        padding: EdgeInsets.all(Dimensions.width20),
+                        child: Icon(
+                          Icons.search,
+                          size: Dimensions.iconSize24,
+                        ),
+                      )),
+                  Expanded(
+                    child: TextField(
+                        onTap: () {
+                          setState(() {
+                            userInput = _textController.text;
+                            apiController.setYear(userInput);
+                            apiController.fetchData();
+                          });
+                        },
+                        textInputAction: TextInputAction.search,
+                        controller: _textController,
+                        decoration: InputDecoration(
+                            hintText: "Select the year!",
+                            hintStyle: TextStyle(
+                              color: Colors.grey,
+                              fontSize: Dimensions.font16,
+                            ),
+                            border: InputBorder.none,
+                            suffixIcon: IconButton(
+                                onPressed: () {
+                                  _textController.clear();
+                                },
+                                icon: const Icon(Icons.clear)
+                            )
+                        )),
+                  ),
+                ],
+              ),
+            ),
+          ),
           //showing the body
           const Expanded(
-              child: SingleChildScrollView(
-                  physics: BouncingScrollPhysics(), child: SpainBodyScreen())
-          ),
+              child: FranceBodyScreen()),
+          SizedBox(height: Dimensions.screenHeight*0.01,)
         ],
       ),
     );
-
   }
 }

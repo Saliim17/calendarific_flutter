26228beeba05b2830854db8fe83f75d7b89b7621
Saliim17 <ahouchsalim@gmail.com>
2022-11-03 16:29:54 +0100

    Dependencies for every screen.

---
+++ lib/dependencies/api_bindings.dart
@@ -0,0 +1,11 @@
+import 'package:calendarific/controllers/api_controller.dart';
+import 'package:get/get.dart';
+
+class ApiBinding extends Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut<ApiController>(
+          () => ApiController(),
+    );
+  }
+}

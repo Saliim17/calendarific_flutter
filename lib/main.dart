241913fc82b49f8bdd4e66f1b6fd0a403091027b
Saliim17 <ahouchsalim@gmail.com>
2022-11-03 16:36:32 +0100

    Clearing the device connection status log.

--- lib/main.dart
+++ lib/main.dart
@@ -2,26 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import 'package:calendarific/routes/route_helper.dart';
-import 'package:connectivity_plus/connectivity_plus.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  var connectivityResult = await (Connectivity().checkConnectivity());
-  if (connectivityResult == ConnectivityResult.mobile) {
-    print("I am connected to a mobile network.");
-  } else if (connectivityResult == ConnectivityResult.wifi) {
-    print("I am connected to a wifi network.");
-  } else {
-    print("I am not connected");
-
-  }
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+  MyApp({super.key});// This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(

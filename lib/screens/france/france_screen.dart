a982f3aac9b39111f659f0891a4b8619d1f37f85
Saliim17 <ahouchsalim@gmail.com>
2022-11-03 16:32:20 +0100

    Minor responsibilities

--- lib/screens/france/france_screen.dart
+++ lib/screens/france/france_screen.dart
@@ -1,94 +1,148 @@
-import 'package:calendarific/screens/france/main_france_screen.dart';
-import 'package:calendarific/screens/spain/spain_screen.dart';
+import 'package:calendarific/screens/france/france_body_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:persistent_bottom_nav_bar_v2/persistent-tab-view.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
 
-class FranceScreen extends StatefulWidget {
+import '../../controllers/api_controller.dart';
+import '../../utils/dimensions.dart';
+import '../../widgets/big_text.dart';
 
-  const FranceScreen({Key? key}) : super(key: key);
+class MainFranceScreen extends StatefulWidget {
+  const MainFranceScreen({Key? key}) : super(key: key);
 
   @override
-  State<FranceScreen> createState() => _FranceScreenState();
-
+  State<MainFranceScreen> createState() => _MainFranceScreenState();
 }
 
-class _FranceScreenState extends State<FranceScreen> {
-  int _selectedIndex = 0;
-  late PersistentTabController _controller;
+class _MainFranceScreenState extends State<MainFranceScreen> {
 
-  List pages=[
-    /*MainFoodPage(),*/
-    Container(child: Center(child: Text("Next page"))),
-    /*CartHistory(),
-    AccountPage(),*/
-  ];
+  ApiController apiController = Get.put(ApiController());
 
-  void onTapNav(int index){
-    setState(() {
-      _selectedIndex = index;
-    });
+  // use this controller to get what the usd typed
+  final _textController = TextEditingController();
+
+  // store user text input into variable
+  String userInput = "";
+  
+  _loadResource (BuildContext context) {
+    
+    if (userInput == "")  {
+      apiController.setYear("2022");
+    } else {
+      apiController.setYear(userInput);
+    }
+    apiController.setCountry("FR");
+    apiController.fetchData();
   }
-
+  
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
-    _controller = PersistentTabController(initialIndex: 0);
-
+    WidgetsBinding.instance
+        .addPostFrameCallback((_) =>_loadResource(context));
   }
 
-  List<Widget> _buildScreens() {
-    return [
-      MainFranceScreen(),
-      SpainScreen(),
-    ];
-  }
-  List<PersistentBottomNavBarItem> _navBarsItems() {
-    return [
-      PersistentBottomNavBarItem(
-        icon: const Icon(CupertinoIcons.flag),
-        title: "France",
-        activeColorPrimary: CupertinoColors.activeBlue,
-        inactiveColorPrimary: CupertinoColors.systemGrey,
-      ),
-      PersistentBottomNavBarItem(
-        icon: const Icon(CupertinoIcons.flag_fill),
-        title: "Spain",
-        activeColorPrimary: CupertinoColors.activeGreen,
-        inactiveColorPrimary: CupertinoColors.systemGrey,
-      ),
-    ];
-  }
   @override
   Widget build(BuildContext context) {
-    return PersistentTabView(
-      context,
-      controller: _controller,
-      screens: _buildScreens(),
-      items: _navBarsItems(),
-      confineInSafeArea: true,
-      backgroundColor: Colors.white, // Default is Colors.white.
-      handleAndroidBackButtonPress: true, // Default is true.
-      resizeToAvoidBottomInset: true, // This needs to be true if you want to move up the screen when keyboard appears. Default is true.
-      stateManagement: true, // Default is true.
-      hideNavigationBarWhenKeyboardShows: true, // Recommended to set 'resizeToAvoidBottomInset' as true while using this argument. Default is true.
-      decoration: NavBarDecoration(
-        borderRadius: BorderRadius.circular(10.0),
-        colorBehindNavBar: Colors.white,
-      ),
-      popAllScreensOnTapOfSelectedTab: true,
-      popActionScreens: PopActionScreensType.all,
-      itemAnimationProperties: const ItemAnimationProperties( // Navigation Bar's items animation properties.
-        duration: Duration(milliseconds: 200),
-        curve: Curves.ease,
-      ),
-      screenTransitionAnimation: const ScreenTransitionAnimation( // Screen transition animation on change of selected tab.
-        animateTabTransition: true,
-        curve: Curves.ease,
-        duration: Duration(milliseconds: 200),
-      ),
-      navBarStyle: NavBarStyle.style2, // Choose the nav bar style with this property.
-    );
+
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      //color set to transperent or set your own color
+      statusBarIconBrightness: Brightness.dark,
+      //set brightness for icons, like dark background light icons
+    ));
+
+    return Scaffold(
+      body: Column(
+          children: [
+            //showing the header
+            Container(
+              color: CupertinoColors.activeBlue,
+              margin: EdgeInsets.only(
+                  top: Dimensions.height45, bottom: Dimensions.height15
+              ),
+              padding: EdgeInsets.only(
+                  top: Dimensions.width10,
+                  left: Dimensions.width20,
+                  right: Dimensions.width20
+              ),
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    BigText(
+                      fontWeight: FontWeight.w900,
+                      text: "CALENDARIFIC.",
+                    ),
+                  ]
+              ),
+            ),
+            Container(
+                margin: EdgeInsets.all(Dimensions.height10),
+                child: BigText(text: "Holidays in France!", fontWeight: FontWeight.w700)
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: Dimensions.height15),
+              child: Container(
+                width: Dimensions.screenWidth * 0.9,
+                height: Dimensions.screenHeight / 14,
+                decoration: BoxDecoration(
+                  color: const Color.fromRGBO(240, 240, 240, 1),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Row(
+                  children: [
+                    InkWell(
+                        onTap: () {
+                          setState(() {
+                            userInput = _textController.text;
+                            apiController.setYear(userInput);
+                            apiController.fetchData();
+                          });
+                        },
+                        child: Padding(
+                          padding: EdgeInsets.all(Dimensions.width20),
+                          child: Icon(
+                            Icons.search,
+                            size: Dimensions.iconSize24,
+                          ),
+                        )),
+                    Expanded(
+                      child: TextField(
+                        onTap: () {
+                          setState(() {
+                            userInput = _textController.text;
+                            apiController.setYear(userInput);
+                            apiController.fetchData();
+                          });
+                        },
+                        textInputAction: TextInputAction.search,
+                        controller: _textController,
+                          decoration: InputDecoration(
+                            hintText: "Select the year!",
+                            hintStyle: TextStyle(
+                              color: Colors.grey,
+                              fontSize: Dimensions.font16,
+                            ),
+                            border: InputBorder.none,
+                            suffixIcon: IconButton(
+                              onPressed: () {
+                                _textController.clear();
+                              },
+                              icon: const Icon(Icons.clear)
+                            )
+                          )),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            //showing the body
+            const Expanded(
+                child: FranceBodyScreen()),
+            SizedBox(height: Dimensions.screenHeight*0.01,)
+          ],
+        ),
+      );
   }
 }

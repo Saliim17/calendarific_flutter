999e67591c23d4e00abbb9c4aacd477897b6faee
Saliim17 <ahouchsalim@gmail.com>
2022-11-03 16:37:30 +0100

    Completed body of the screen

--- lib/screens/spain/spain_body_screen.dart
+++ lib/screens/spain/spain_body_screen.dart
@@ -1,4 +1,9 @@
+import 'package:calendarific/controllers/api_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:get/get.dart';
+import '../../size_config.dart';
+import '../home_screen.dart';
 
 class SpainBodyScreen extends StatefulWidget {
   const SpainBodyScreen({Key? key}) : super(key: key);
@@ -8,8 +13,39 @@ class SpainBodyScreen extends StatefulWidget {
 }
 
 class _SpainBodyScreenState extends State<SpainBodyScreen> {
+
+  ApiController apiController = HomeScreen.getApiController();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    SizeConfig().init(context);
+    return Obx(
+          () => apiController.isLoading.value
+          ? const Center(child: CircularProgressIndicator(),)
+          : ListView.builder(
+          scrollDirection: Axis.vertical,
+          shrinkWrap: true,
+          itemCount: apiController.jsonModel?.response?.holidays.length ?? 0,
+          itemBuilder: (context, index) {
+            final date = DateTime.parse(apiController.jsonModel?.response?.holidays[index].date?.iso);
+            String dateFormatted = DateFormat.yMMMMd('en_US').format(date);
+            return ListTile(
+              title: Text(
+                  apiController.jsonModel?.response?.holidays[index].name ?? 'no name'
+              ),
+              subtitle: Text(
+                  apiController.jsonModel?.response?.holidays[index].description ?? 'no description'
+              ),
+              leading: Text(
+                  dateFormatted
+              ),
+            );
+          }
+      ),
+    );
   }
 }
+
+
+
+

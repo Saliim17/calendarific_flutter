6f63ba22288fbe8705341ec403e97833a561f914
Saliim17 <ahouchsalim@gmail.com>
2022-11-13 13:16:19 +0100

    xd

--- lib/dependencies/api_bindings.dart
+++ lib/dependencies/api_bindings.dart
@@ -3,11 +3,10 @@ import 'package:calendarific/controllers/api_controller.dart';
 import 'package:get/get.dart';
 
 class ApiBinding extends Bindings {
-
   @override
   void dependencies() {
     Get.lazyPut<ApiController>(
-          () => ApiController(),
+      () => ApiController(),
     );
   }
 }
